b133b49b9d2d8c5084fa894a166f918341c5ec19
Moritz Meyer <moritzmeyer@Moritzs-MacBook-Pro.fritz.box>
2021-03-30 11:35:57 +0200

    Implemented Konto.java for OOP

--- src/Bank.java
+++ src/Bank.java
@@ -1,80 +1,69 @@
 public class Bank {
-    private final int MAX_KONTEN = 100;
-    private int anzahl = 0;
-    private float[] konten = new float[MAX_KONTEN];
-    private String[] inhaber = new String[MAX_KONTEN];
-    private final float ZINSSATZ = 3.0f;
-    private int monat = 0;
 
-    private int kontoSuchen(String kunde) {
-        for (int i = 0; i < anzahl; i++) {
-            if (inhaber[i].equals(kunde)) {
-                return i;
+    private final int MAX_KONTEN = 100;
+    private Konto[] konten = new Konto[MAX_KONTEN];
+    private int belegteKonten = 0;
+    private int monat = 0;
+    
+    private Konto kontoSuchen(String kunde) {
+        for (int i = 0; i < belegteKonten; i++) {
+            if (konten[i].getInhaber().equals(kunde)) {
+                return konten[i];
             }
         }
-        return -1;
+        return null;
     }
 
     public void kontoEroeffnen(String kunde) {
-        if (kontoSuchen(kunde) != -1) {
-            System.out.println(kunde + " hat schon ein Konto.");
+        Konto konto = kontoSuchen(kunde);
+        if (konto != null) {
+            System.out.println(konto.getInhaber() + " hat schon ein Konto.");
             return;
         }
-        if (anzahl >= MAX_KONTEN) {
+        if (belegteKonten >= MAX_KONTEN) {
             System.out.println("Keine Konten mehr frei");
             return;
         }
-        inhaber[anzahl] = kunde;
-        konten[anzahl] = 0;
+        
+        konten[belegteKonten] = new Konto(kunde);
         System.out.println("Das Konto für " + kunde + " wurde angelegt.");
-        anzahl++;
+        belegteKonten++;
     }
 
     public void einzahlen(String kunde, float betrag) {
-        int kontonr = kontoSuchen(kunde);
-        if (kontonr == -1) {
+        Konto konto = kontoSuchen(kunde);
+        if (konto == null) {
             System.out.println(kunde + "besitzt kein Konto.");
         } else {
-            konten[kontonr] = konten[kontonr] + betrag;
+            konto.einzahlen(betrag);
             System.out.println(
-                    kunde + " hat " + betrag + " € eingezahlt und verfuegt nun ueber " + konten[kontonr] + " €");
+                    kunde + " hat " + betrag + " € eingezahlt und verfuegt nun ueber " + konto.getKontostand() + " €");
         }
     }
 
     public float abheben(String kunde, float betrag) {
-        int kontonr = kontoSuchen(kunde);
-        if (kontonr == -1) {
+        Konto konto = kontoSuchen(kunde);
+        if (konto == null) {
             System.out.println(kunde + " besitzt kein Konto.");
             return 0;
         } else {
-            if (konten[kontonr] > betrag) {
-                konten[kontonr] = konten[kontonr] - betrag;
-                System.out.println(
-                        kunde + "hat " + betrag + " € abgehoben und verfuegt nun ueber " + konten[kontonr] + " €");
-            } else {
-                betrag = konten[kontonr];
-                konten[kontonr] = konten[kontonr] - konten[kontonr];
-                System.out.println(
-                        kunde + "verfuegt nicht über ausreichend Guthaben, deshalb wurde der max. Betrag von " + betrag
-                                + " € abgehoben und " + kunde + " verfuegt nun ueber " + konten[kontonr] + " €");
-            }
-            return betrag;
+            return konto.abheben(betrag);
         }
     }
 
     public void bestandAusgeben() {
         System.out.println("Nach dem " + monat + ". Monat befinden sich folgende Betraege auf den Konten:");
         float summe = 0;
-        for (int i = 0; i < anzahl; i++) {
-            System.out.println(inhaber[i] + ": " + konten[i] + " €");
-            summe = summe + konten[i];
+        for (int i = 0; i < belegteKonten; i++) {
+            System.out.println(konten[i].toSring());
+            summe = summe + konten[i].getKontostand();
         }
         System.out.println("=> Auf allen Konten in Summe befinden sich: " + summe + " €");
     }
 
     public void abschlussDurchfuehren() {
-        for (int i = 0; i < anzahl; i++) {
-            konten[i] = konten[i] * (1 + ZINSSATZ / 12 / 100);
+        for (int i = 0; i < belegteKonten; i++) {
+            konten[i].verzinsen();
         }
         monat++;
     }
---
+++ src/Konto.java
@@ -0,0 +1,47 @@
+public class Konto {
+    private String inhaber;
+    private float kontostand;
+    private final float ZINSSATZ = 3.0f;
+
+    public Konto(String inhaber) {
+        this.inhaber = inhaber;
+    }
+
+    public String getInhaber() {
+        return inhaber;
+    }
+
+    public float getKontostand() {
+        return kontostand;
+    }
+
+    public void einzahlen (float betrag) {
+        kontostand += betrag;
+    }
+
+    public float abheben (float betrag) {
+        if (kontostand > betrag) {
+            kontostand -= betrag;
+            System.out.println(
+                        inhaber + "hat " + betrag + " € abgehoben und verfuegt nun ueber " + kontostand + " €");
+        } else {
+            betrag = kontostand;
+            kontostand -= betrag;
+            System.out.println(
+                        inhaber + "verfuegt nicht über ausreichend Guthaben, deshalb wurde der max. Betrag von " + betrag
+                                + " € abgehoben und " + inhaber + " verfuegt nun ueber " + kontostand + " €");
+        }
+        return betrag;
+    }
+
+    public String toSring() {
+        return inhaber + ": " + kontostand;
+    }
+
+    public float verzinsen() {
+        kontostand = kontostand * (1 + ZINSSATZ / 12 / 100);
+        return kontostand;
+    }
+
+
+}
